7bc249552c191932ed6ac4accdf170a55ea4848a
unknown <Hiru1728>
2022-07-31 23:14:01 +0600

    checked Array length

---
+++ variable.js
@@ -0,0 +1,30 @@
+// Number Type Variable
+var sunglassPrice = 120;
+// String Type Variable
+var specialOneName = "Ariana Grande";
+
+// boolean type variable
+var isSingle = true;
+
+// Two Types of number : Integer :120,
+// float, decimal: 3.65,
+
+// variable naming convention: rules, best practice
+/**
+ * 1. single word
+ * 2. no gap or speace
+ * 3.no quote
+ * 4. no keyword
+ * 5. can not start with a number
+ * 6. can not contain dash
+ * 7. should use camelCase
+ * */ 
+
+// Math Operation: +, -,*,/
+// Shorthand: +=,-=,++,--
+
+// convert: parseInt, parseFloat
+
+// tofixed
+
+// remainder: vagses: %
