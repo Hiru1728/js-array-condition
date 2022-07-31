c876e7aac381b89d39bad3a03b19b235a4deada0
unknown <Hiru1728>
2022-07-31 23:37:51 +0600

    index added

---
+++ index.js
@@ -0,0 +1,13 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+//1. get element value by index
+var second = numbers[1];
+// console.log(second);
+//2. set element value by index
+numbers[1] = 77;
+numbers[3] = 44;
+// console.log(numbers);
+
+// 3.Find index of a element
+var postionIndex = numbers.indexOf(89);
+console.log(postionIndex);
