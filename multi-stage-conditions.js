1c1fd15fe9c774d68167cd01466c3eee2277ac69
unknown <Hiru1728>
2022-08-01 01:48:27 +0600

    multi stage and nested condition added

---
+++ multi-stage-conditions.js
@@ -0,0 +1,16 @@
+var money = 30;
+var danisPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+if (danisPrice < money) {
+    console.log('I can eat danish');
+}
+else if (butterBread < money) {
+    console.log('I can eat ButterBrad');
+}
+else if (toastBiscuit < money) {
+    console.log('I will eat ToastBiscuit');
+}
+else {
+    console.log('Only drink tea that is good for health');
+}
---
+++ nested-conditions.js
@@ -0,0 +1,17 @@
+var math = true;
+var geometry = true;
+var straightline = false;
+
+if (math == true) {
+    if (geometry == true) {
+        if (straightline == true) {
+
+        }
+        else {
+            console.log('I not want on the back side');
+        }
+    }
+    else {
+        console.log('I can not become a Phitagurash.')
+    }
+}
