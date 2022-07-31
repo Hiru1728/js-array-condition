44865aaa25aba7f2c1391afd0dca53f3b3f9eeda
unknown <Hiru1728>
2022-08-01 01:33:58 +0600

    multiple condition

---
+++ conditional.js
@@ -0,0 +1,39 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+
+// if iphone price is less than my buget. I will buy the iphone.
+/**
+ * if iphone price is less than my buget
+ *  I will buy the iphone
+ * 
+ * 
+ * if(condition){
+ *       //will execute if the condition is true
+ * }
+ * 
+ */
+
+if (iphonePrice < myBudget) {
+    console.log('I will be buy iphone');
+}
+var chickenPrice = 500;
+var myMoney = 50;
+
+// if (chickenPrice <= myMoney) {
+//     console.log('I will eat cook chicken ');
+// }
+// if (chickenPrice > myMoney) {
+//     console.log('I will be eat Smashed potato and lentils soup');
+// }
+/**
+ * if chicken price is less than my budget
+ * i will eat chicken
+ * or : else
+ */
+
+if (chickenPrice < myMoney) {
+    console.log('I will eat chicken');
+}
+else {
+    console.log('I will eat potato');
+}
---
+++ multiple-condition.js
@@ -0,0 +1,40 @@
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
+// if (isGraduated === true) {
+//     console.log('i will go to University');
+// }
+// else {
+//     console.log('I will not go to university');
+// }
+
+// if (isGraduated === true && salary > 50000) {
+//     console.log('i will go to University');
+// }
+// else {
+//     console.log('I will not go to university');
+// }
+// if (isGraduated === true && salary > 50000 && cars >= 1) {
+//     console.log('i will go to University');
+// }
+// else {
+//     console.log('I will not go to university');
+// }
+
+var isGraduated = false;
+var salary = 31000;
+var cars = 1;
+
+// if (isGraduated === true || salary > 50000) {
+//     console.log('I will best try for exam');
+// }
+// else {
+//     console.log('I will not best try for exam');
+// }
+
+if (isGraduated === true && salary > 50000 || cars >= 1) {
+    console.log('I will best try for exam');
+}
+else {
+    console.log('I will not best try for exam');
+}
