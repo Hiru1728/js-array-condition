0adb86b7f3919ce6b5c22bcc0208bf66c3fb617b
unknown <Hiru1728>
2022-08-01 00:31:59 +0600

    push and pop

--- push-pop.js
+++ push-pop.js
@@ -1,2 +1,19 @@
 var numbers = [78, 45, 98, 45];
+// use push to add element to an array as the last element array
+// numbers.push(63);
+//console.log(numbers);
+
+var friend = ['kalam', 'salam'];
+friend.push('gelam');
+friend.push('pailam');
+console.log(friend);
+numbers.push(numbers[0] = 63);
+// Use pop to get last element
+console.log(numbers);
+numbers.pop(numbers[1]);
+//var element = numbers.pop();
+console.log(numbers);
+//console.log(element);
+product = numbers.shift();
+console.log(product);
 console.log(numbers);
